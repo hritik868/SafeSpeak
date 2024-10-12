daae6347027a0db3663eaee136d765324114082f
PM <121531128+26-PM@users.noreply.github.com>
2024-10-12 16:05:16 +0000

    fixed small errors

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./reports.css"; // Assuming you have a CSS file for styles
+import "./reports.css"; 
 
 const ReportsPage = () => {
   const [reports, setReports] = useState([]);
